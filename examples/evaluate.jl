3540c57108bd30e7c8e53700fd03862673fe5d7d
Adrian Grupp <adrian.grupp@posteo.net>
2022-02-02 10:56:48 +0100

    evaluation script for PCE coefficients

--- examples/evaluate.jl
+++ examples/evaluate.jl
@@ -6,7 +6,24 @@ function compareCoefficients(file1::String, file2::String)
     f1 = load(file1)
     f2 = load(file2)
 
-    println(f1)
+    println("Comparing PCE coefficients between $(basename(file1)) and $(basename(file2))")
+
+    coeffs1 = f1["pf_state"]
+    coeffs2 = f2["pf_state"]
+
+    # Loop over all PCE coefficient sets 
+    for (key, val) in coeffs1
+        if haskey(coeffs2, key)
+            mat1, mat2 = val, coeffs2[key] # coefficients are stored as matrices
+
+            diff = mat1 - mat2 # difference between coefficients
+            # compare coefficients rowwise, i.e. for each bus
+            for (i, row) in enumerate(eachrow(diff))
+                println("∞-Norm for $key, $i:\t ", norm(diff, Inf))
+            
+            end
+        end
+    end
 end
 
 
