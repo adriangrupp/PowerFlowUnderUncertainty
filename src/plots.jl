05976456b1386c441ef683ef53b2fa829208938a
Adrian Grupp <adrian.grupp@posteo.net>
2022-03-14 17:03:08 +0100

    larger plot

--- src/plots.jl
+++ src/plots.jl
@@ -1,7 +1,8 @@
 export  plotHistogram,
         plotHistogram_bus,
         plotHistogram_nodal,
-        plotHistogram_branch
+        plotHistogram_branch,
+        plotHistogram_gen30
 
 function plotHistogram(x::Vector; kwargs...)
     bins = haskey(kwargs, :bins) ? kwargs[:bins] : 70
@@ -59,4 +60,24 @@ function plotHistogram_branch(i::Matrix, name::String; kwargs...)
     println("Plotting: $dir/$name.pdf")
     savefig("plots/$name.pdf")
     clf() 
+end
+
+# Plots for all branches at once (5 branches)
+function plotHistogram_gen30(i::Matrix, name::String, dir::String; kwargs...)
+    haskey(kwargs, :fignum) ? figure(kwargs[:fignum]) : figure()
+    plt[:subplot](331)
+    plotHistogram(i[1, :]; xlabel = name * "_1", ylabel = "ρ(" * name * "_1)", kwargs...)
+    plt[:subplot](333)
+    plotHistogram(i[2, :]; xlabel = name * "_2", ylabel = "ρ(" * name * "_2)", kwargs...)
+    plt[:subplot](334)
+    plotHistogram(i[3, :]; xlabel = name * "_3", ylabel = "ρ(" * name * "_3)", kwargs...)
+    plt[:subplot](336)
+    plotHistogram(i[4, :]; xlabel = name * "_4", ylabel = "ρ(" * name * "_4)", kwargs...)
+    plt[:subplot](337)
+    plotHistogram(i[5, :]; xlabel = name * "_5", ylabel = "ρ(" * name * "_5)", kwargs...)
+    plt[:subplot](339)
+    plotHistogram(i[6, :]; xlabel = name * "_6", ylabel = "ρ(" * name * "_6)", kwargs...)
+    println("Plotting: $dir/$name.pdf")
+    savefig("$dir/$name.pdf")
+    clf()
 end
