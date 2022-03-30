2afa2a68f27eca3a72a30566b9f8555cd9476957
Adrian Grupp <adrian.grupp@posteo.net>
2022-03-30 16:40:25 +0200

    30 bus: add 10 unc experiment non-inrusive

--- examples/30-Bus_example/init_ni.jl
+++ examples/30-Bus_example/init_ni.jl
@@ -72,7 +72,7 @@ function initUncertainty_1u(p, q)
     op = Beta01OrthoPoly(maxDeg, α, β)
 
     println("Polynomial basis of size $(op.deg+1):")
-    showbasis(op, digits = 2)
+    showbasis(op, digits=2)
     println()
 
     # PCE of demand. Compute affine coefficients for univariate uncertainty. Transform support of distributions
@@ -81,8 +81,8 @@ function initUncertainty_1u(p, q)
     δ = 0.15 # deviation from nominal value
     lp, up = [1 - δ, 1 + δ] * p
     lq, uq = [1 - δ, 1 + δ] * q
-    pd[1, [1, 2]] = supp2pce(α, β) * [lp; up]
-    qd[1, [1, 2]] = supp2pce(α, β) * [lq; uq]
+    pd[1, [1, 2]] = convert2affinePCE(lp, up, op)
+    qd[1, [1, 2]] = convert2affinePCE(lq, uq, op)
 
     samples = sampleMeasure(numSamples, op)  # NI model samples
     samples_p = samples * (up - lp) .+ lp
@@ -90,12 +90,12 @@ function initUncertainty_1u(p, q)
     ξ = sampleMeasure(5000, op)  # Evaluation samples
 
     return Dict(:opq => op,
-            :dim => op.deg + 1,
-            :pd => pd,
-            :qd => qd,
-            :samples_unc => samples,    # samples for exogenous uncertainty
-            :samples_bus => hcat(samples_p, samples_q), # samples for bus uncertainty
-            :ξ => ξ)
+        :dim => op.deg + 1,
+        :pd => pd,
+        :qd => qd,
+        :samples_unc => samples,    # samples for exogenous uncertainty
+        :samples_bus => hcat(samples_p, samples_q), # samples for bus uncertainty
+        :ξ => ξ)
 end
 
 """
@@ -130,13 +130,66 @@ function initUncertainty_2u(p::Vector, q::Vector)
     for (i, op) in enumerate(ops)
         lp, up = [1 - δ, 1 + δ] * p[i]
         lq, uq = [1 - δ, 1 + δ] * q[i]
-        pce_p = supp2pce(α[i], β[i]) * [lp; up]
+        pce_q = supp2pce(α[i], β[i]) * [lq; uq] # need to do this due to 0/0 support
+        pce_p = convert2affinePCE(lp, up, op)
+    
+        pdi = assign2multi(pce_p, i, mop.ind) # Get sparse array assigned with first two pce coefficients to multivariate polynomial
+        qdi = assign2multi(pce_q, i, mop.ind)
+        pd[i, :] = pdi' # Collect index/coefficient arrays for all uncertainties
+        qd[i, :] = qdi'
+    
+        samples_p[:, i] = samples[:, i] * (up - lp) .+ lp # Generate samples for each uncertainty
+        samples_q[:, i] = samples[:, i] * (uq - lq) .+ lq # Generate samples for each uncertainty
+    end
+
+    return Dict(:opq => mop,
+            :dim => mop.dim,
+            :pd => pd, # PCE of initially uncertain buses TODO: currently this encompasses gens and loads
+            :qd => qd,
+            :samples_unc => samples,                    # samples for exogenous uncertainties
+            :samples_bus => hcat(samples_p, samples_q), # samples for bus uncertainties, first all p then all q values
+            :ξ => ξ)
+end
+
+"""
+ Initialization for various uncertainties (β-distributions)
+"""
+function initUncertainty_Nu(p::Vector, q::Vector)
+    n = 10
+
+    # Setup multivariate basis
+    α = [2, 3, 3, 4, 2, 1, 5, 3, 2, 3]
+    β = [4.666, 3, 2, 2, 3, 1, 4, 4, 3, 3]
+    ops = [Beta01OrthoPoly(maxDeg, α[i], β[i]; Nrec = maxDeg+2) for i in 1:n]
+
+    mop = MultiOrthoPoly(ops, maxDeg)
+    println("Polynomial basis:")
+    show(mop)
+    println()
+
+    # Sample uncertainties for NI model (samples) and postprocessing (ξ)
+    samples = [sampleMeasure(numSamples, op) for op in ops]
+    samples = hcat(samples...) # numSamples x nUnc matrix
+    ξ = [sampleMeasure(5000, op) for op in ops]  # Evaluation samples for each uncertainty
+    ξ = hcat(ξ...) # 5000 x nUnc
+
+    # PCE of demand. Compute affine coefficients for univariate uncertainty. Transform support of distributions
+    pd = zeros(nUnc, mop.dim) # matrix storing all pd PCE coefficients
+    qd = zeros(nUnc, mop.dim)
+    samples_p = zeros(numSamples, nUnc) # matrix storing all p-samples
+    samples_q = zeros(numSamples, nUnc) 
+    δ = 0.15 # deviation from nominal value
+    # Iterate for all uncertainties
+    for (i, op) in enumerate(ops)
+        lp, up = [1 - δ, 1 + δ] * p[i]
+        lq, uq = [1 - δ, 1 + δ] * q[i]
+        pce_p = convert2affinePCE(lp, up, op)
         pce_q = supp2pce(α[i], β[i]) * [lq; uq]
         pdi = assign2multi(pce_p, i, mop.ind) # Get sparse array assigned with first two pce coefficients to multivariate polynomial
         qdi = assign2multi(pce_q, i, mop.ind)
         pd[i, :] = pdi' # Collect index/coefficient arrays for all uncertainties
         qd[i, :] = qdi'
-
+    
         samples_p[:, i] = samples[:, i] * (up - lp) .+ lp # Generate samples for each uncertainty
         samples_q[:, i] = samples[:, i] * (uq - lq) .+ lq # Generate samples for each uncertainty
     end
---
+++ examples/30-Bus_example/main_acpf_10u_ni.jl
@@ -0,0 +1,136 @@
+using PowerFlowUnderUncertainty, PowerModels, Ipopt, JuMP, JLD
+
+"""
+30 Bus net: Non-intrusive PCE for stochastic power flow.
+10 Uncertainties: Bus 3(l2), 4(l3), 7(l4), 8(l5), 10(l6), 17(l11), 19(l13), 29(l19), 30(l20)  and Bus 13(g6).
+Take samples of power values, compute PF, perform regression for all needed variables.
+"""
+
+caseFile = "case30.m"
+numSamples = 500
+maxDeg = 3
+nUnc = 10
+postProcessing = true
+
+println("\n\t\t===== Stochastic Power Flow: 30 Bus case, 10 Uncertainties, non-intrusive PCE =====\n")
+
+## Read case file, initialize network uncertainties and corresponding values
+include("init_ni.jl")
+network_data = readCaseFlie(caseFile)
+sys = parseNetworkData(network_data)
+# Define uncertain buses
+p = [sys[:Pd][2],
+     sys[:Pd][3], 
+     sys[:Pd][4], 
+     sys[:Pd][5], 
+     sys[:Pd][6], 
+     sys[:Pd][11], 
+     sys[:Pd][13], 
+     sys[:Pd][19], 
+     sys[:Pd][20], 
+     sys[:Pg][6]]
+q = [sys[:Qd][2],
+     sys[:Qd][3], 
+     sys[:Qd][4], 
+     sys[:Qd][5], 
+     sys[:Qd][6], 
+     sys[:Qd][11], 
+     sys[:Qd][13], 
+     sys[:Qd][19], 
+     sys[:Qd][20], 
+     sys[:Qg][6]]
+unc = initUncertainty_Nu(p, q)
+
+## Dict of simulation results: each row of a parameter describes a bus
+pfRes = Dict(:pg => Array{Float64}(undef, sys[:Ng], 0),
+    :qg => Array{Float64}(undef, sys[:Ng], 0),
+    :e => Array{Float64}(undef, sys[:Nbus], 0),
+    :f => Array{Float64}(undef, sys[:Nbus], 0)
+)
+
+## Initialize solver
+solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 2)
+
+## Execute the model for all samples
+println("Running $numSamples deterministic PF calculations (model evalutations)...")
+@time begin
+    for x in eachrow(unc[:samples_bus]) # each row is a sample set
+        network_data["load"]["2"]["pd"] = x[1]      # bus 3 / load 2
+        network_data["load"]["2"]["qd"] = x[nUnc+1] # second half of matrix are q values
+        network_data["load"]["3"]["pd"] = x[2]      
+        network_data["load"]["3"]["qd"] = x[nUnc+2] 
+        network_data["load"]["4"]["pd"] = x[3]      
+        network_data["load"]["4"]["qd"] = x[nUnc+3] 
+        network_data["load"]["5"]["pd"] = x[4]      # bus 8 / load 5
+        network_data["load"]["5"]["qd"] = x[nUnc+4] 
+        network_data["load"]["6"]["pd"] = x[5]      
+        network_data["load"]["6"]["qd"] = x[nUnc+5] 
+        network_data["load"]["11"]["pd"] = x[6]      
+        network_data["load"]["11"]["qd"] = x[nUnc+6] 
+        network_data["load"]["13"]["pd"] = x[7]      
+        network_data["load"]["13"]["qd"] = x[nUnc+7] 
+        network_data["load"]["19"]["pd"] = x[8]      
+        network_data["load"]["19"]["qd"] = x[nUnc+8] 
+        network_data["load"]["20"]["pd"] = x[9]      
+        network_data["load"]["20"]["qd"] = x[nUnc+9] 
+        network_data["gen"]["6"]["pg"]  = x[10]      # bus 13 / gen 6, no q values vor generators
+
+        res = runModel(network_data, solver) # pass modified network data
+
+        pfRes[:pg] = hcat(pfRes[:pg], res[:pg])
+        pfRes[:qg] = hcat(pfRes[:qg], res[:qg])
+        pfRes[:e] = hcat(pfRes[:e], res[:e])
+        pfRes[:f] = hcat(pfRes[:f], res[:f])
+    end
+    print("Finished. Time:")
+end
+
+# Perform the regression for PCE coefficients on pd, qd, e and f
+println("\nCompute non-intrusive PCE coefficients...\n")
+pce = computeCoefficientsNI(unc[:samples_unc], pfRes, unc)
+
+# Get additional PCE of currents, branch flows and demands
+pf_state = getGridStateNonintrusive(pce, sys, unc)
+println("PCE coefficients:")
+display(pf_state)
+
+# Sample for parameter values, using their PCE representations
+pf_samples = generateSamples(unc[:ξ], pf_state, sys, unc)
+println("\nPCE model evaluations for $(size(unc[:ξ])[1]) samples ξ:")
+display(pf_samples)
+println()
+
+
+### Store data for evaluation ###
+
+# PCE coefficients
+f_coeff = "coefficients/SPF_10u_NI.jld"
+save(f_coeff, "pf_state", pf_state)
+println("PCE coefficients data saved to $f_coeff.\n")
+
+# Compute and store moments from PCE coefficients
+moments = computeMoments(pf_state, unc)
+f_moms = "coefficients/SPF_10u_NI_moments.jld"
+save(f_moms, "moments", moments)
+println("PCE moments data saved to $f_moms.\n")
+
+
+
+### POST PROCESSING ###
+if postProcessing
+    mycolor = "red"
+    plotHistogram_6in9(pf_samples[:pg], "pg", "./plots/10u_non-intrusive"; fignum=1 + 10, color=mycolor)
+    plotHistogram_6in9(pf_samples[:qg], "qg", "./plots/10u_non-intrusive"; fignum=2 + 10, color=mycolor)
+
+    plotHistogram_9in9(pf_samples[:e][1:9, :], "e1", "./plots/10u_non-intrusive"; fignum=3 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:e][10:18, :], "e2", "./plots/10u_non-intrusive"; fignum=4 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:e][19:27, :], "e3", "./plots/10u_non-intrusive"; fignum=5 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:e][28:30, :], "e4", "./plots/10u_non-intrusive"; fignum=6 + 10, color=mycolor)
+
+    plotHistogram_9in9(pf_samples[:f][1:9, :], "f1", "./plots/10u_non-intrusive"; fignum=7 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:f][10:18, :], "f2", "./plots/10u_non-intrusive"; fignum=8 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:f][19:27, :], "f3", "./plots/10u_non-intrusive"; fignum=9 + 10, color=mycolor)
+    plotHistogram_9in9(pf_samples[:f][28:30, :], "f4", "./plots/10u_non-intrusive"; fignum=10 + 10, color=mycolor)
+    # Plot P & Q of uncertainties TODO
+    plotHistogram_2unc(pf_samples[:pd], pf_samples[:qd], ["pd_8", "qd_8", "pg_13", "qg_13"], "./plots/2u_non-intrusive"; fignum=0 + 10, color=mycolor)
+end
