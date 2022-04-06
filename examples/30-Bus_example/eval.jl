4f8663447f152d580cf18cb8af354ee4cf669e99
Adrian Grupp <adrian.grupp@posteo.net>
2022-04-06 10:45:17 +0200

    add evaluation scenarios

--- examples/30-Bus_example/eval.jl
+++ examples/30-Bus_example/eval.jl
@@ -1,8 +1,21 @@
+using PowerFlowUnderUncertainty, JLD
+
 # Compare PCE coefficients
-# compareCoefficients("coefficients/SPF_intrusive.jld", "coefficients/SPF_sparse.jld")
-# compareCoefficients("coefficients/SPF_intrusive.jld", "coefficients/SPF_NI.jld")
-# compareCoefficients("coefficients/SPF_NI_2unc.jld", "coefficients/SPF_sparse_2unc.jld")
+# 1u: 
+compareCoefficients("coefficients/SPF_NI.jld", "coefficients/SPF_sparse.jld")
+# 2u:
+compareCoefficients("coefficients/SPF_2u_NI.jld", "coefficients/SPF_2u_sparse.jld")
+# 10u:
+compareCoefficients("coefficients/SPF_10u_NI.jld", "coefficients/SPF_10u_sparse.jld")
+
 
 # Compare Moments with MC
+# 1u:
 compareToMCMoments("coefficients/SPF_MC_moments.jld", "coefficients/SPF_NI_moments.jld")
 compareToMCMoments("coefficients/SPF_MC_moments.jld", "coefficients/SPF_sparse_moments.jld")
+# 2u:
+compareToMCMoments("coefficients/SPF_2u_MC_moments.jld", "coefficients/SPF_2u_NI_moments.jld")
+compareToMCMoments("coefficients/SPF_2u_MC_moments.jld", "coefficients/SPF_2u_sparse_moments.jld")
+# 10u:
+compareToMCMoments("coefficients/SPF_10u_MC_moments.jld", "coefficients/SPF_10u_NI_moments.jld")
+compareToMCMoments("coefficients/SPF_10u_MC_moments.jld", "coefficients/SPF_10u_sparse_moments.jld")
